0e93542ae42a0b965b0c156f141fa4db906fe3e7
Julia <byjuliaiglesias@gmail.com>
2025-08-30 15:21:26 -0300

    att link

--- developer-portifolio-json/assets/js/api.js
+++ developer-portifolio-json/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/juiglesiass/ProjetosDIO_Santander_Front-End/refs/heads/main/developer-portifolio-json/data/profile.json';
+    const url = 'https://github.com/juiglesiass/ProjetosDIO_Santander_Front-End/blob/main/developer-portifolio-json/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
